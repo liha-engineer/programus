77e2b8af2b091011bffabec1a6a3f4555bc60e07
LIHA <122168823+liha-engineer@users.noreply.github.com>
2023-01-28 19:54:10 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.90 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,12 +1,6 @@
 class Solution {
     public String solution(String phone_number) {
-        
-        String conceal = "";
-        for (int i = 0; i < phone_number.length() - 4; i++ ) {
-            conceal += "*";
-        }
-        String reveal = phone_number.substring(phone_number.length() - 4);
-        String answer = conceal + reveal;
+        String answer = "";
         return answer;
     }
 }
